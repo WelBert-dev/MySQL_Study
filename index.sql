91c1f82b709bd0fc1d9fc46578e789eaadb35577
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-07 02:34:13 -0300

    Update index.sql

--- index.sql
+++ index.sql
@@ -2,11 +2,11 @@
 
 
 -- DDL: Data Definition Language
-   -- Conceituando: linguagem de manipulação de dados, define um grupo de comandos que são utilizados para manipular os dados no banco.
+   -- Conceituando: linguagem de difinição de dados, define um grupo de comandos que são utilizados para definir as databases e tabelas.
    -- Comandos:
-      -- INSERT
-      -- UPDATE
-      -- DELETE
+      -- CREATE
+      -- DROP
+      -- ALTER
 
 -- DQL: Data Query Language
    -- Conceituando: linguagem de consulta de dados, define uma forma de acesso a consulta de dados no banco. O comando para obter os resultados desejados é:
@@ -15,7 +15,11 @@
    -- Obs: Embora este grupo trabalhe em torno do comando SELECT, existem diversas outras cláusulas que agregam a este comando e será utilizado na pasta separada!
 
 -- DML: Data Manipulation Language
-   -- Conceituando:
+   -- Conceituando: linguagem de manipulação de dados, define um grupo de comandos que são utilizados para manipular os dados no banco.
+   -- Comandos:
+      -- INSERT
+      -- UPDATE
+      -- DELETE
 
 -- DCL: Data Control Language
    -- Conceituando:
