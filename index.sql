a2b9e46debade5830963e0fce8a830a2aba87ef4
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-07 03:01:58 -0300

    Update index.sql

--- index.sql
+++ index.sql
@@ -14,7 +14,7 @@
    -- Conceituando: linguagem de consulta de dados, define uma forma de acesso a consulta de dados no banco. O comando para obter os resultados desejados é:
    -- Comandos:
       -- SELECT
-   -- Obs: Embora este grupo trabalhe em torno do comando SELECT, existem diversas outras cláusulas que agregam a este comando e será utilizado na pasta separada!
+         -- Obs: Embora este grupo trabalhe em torno do comando SELECT, existem diversas outras cláusulas que agregam a este comando e será utilizado na pasta separada!
 
 -- DML: Data Manipulation Language
    -- Conceituando: linguagem de manipulação de dados, define um grupo de comandos que são utilizados para manipular os dados no banco.
