ea5cb78ed1849d334679a3759ee42d740fde3ecb
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-07 03:05:26 -0300

    Update funcoesDeAgregacao.sql

--- DQL/funcoesDeAgregacao.sql
+++ DQL/funcoesDeAgregacao.sql
@@ -1,1 +1,12 @@
 -- Conceituando:
+
+
+-- COUNT() = Conta ocorrências.
+
+-- MAX() = Retorna o maior valor de uma coluna.
+
+-- MIN() = Retorna o menor valor de uma coluna.
+
+-- AVG() = Retorna a média dos valores de uma coluna.
+
+-- SUM() = Soma todos os valores de uma coluna.
