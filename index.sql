5fe28260c9250e30fabb5e4937fde94efd22146d
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-07 02:13:16 -0300

    Create index.sql

---
+++ index.sql
@@ -0,0 +1,17 @@
+-- Conceituando: 
+
+
+-- DDL: Data Definition Language
+   -- Conceituando:
+
+-- DQL: Data Query Language
+   -- Conceituando:
+
+-- DML: Data Manipulation Language
+   -- Conceituando:
+
+-- DCL: Data Control Language
+   -- Conceituando:
+
+-- TCL: Transaction Control Language
+   -- Conceituando:
