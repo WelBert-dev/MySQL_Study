a18812916be5aa1de6bec4e1ddabc5fc38746ec1
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-07 02:43:09 -0300

    Update index.sql

--- index.sql
+++ index.sql
@@ -4,9 +4,12 @@
 -- DDL: Data Definition Language
    -- Conceituando: linguagem de difinição de dados, define um grupo de comandos que são utilizados para definir as databases e tabelas.
    -- Comandos:
-      -- CREATE
-      -- DROP
-      -- ALTER
+      -- CREATE: utilizado para criar database, ou objetos (like table, index, function, views, store procedure, and triggers).
+      -- DROP: utilizado para deletar objetos da database.
+      -- ALTER: utilizado para alterar a estrutura da database.
+      -- TRUNCATE: utilizado para remover todos os registros da tabela.
+      -- COMMENT: utilizado para adicionar comentários nos dicionários dos dados.
+      -- RENAME: utilizado para renomear um objeto existente na database.
 
 -- DQL: Data Query Language
    -- Conceituando: linguagem de consulta de dados, define uma forma de acesso a consulta de dados no banco. O comando para obter os resultados desejados é:
