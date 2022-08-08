54ea82f03322dd178309b4f70ffc418145e71b4b
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-08 02:21:34 -0300

    Delete funcoesDeAgregacao.sql

--- introduction/funcoesDeAgregacao.sql
+++
@@ -1,9 +0,0 @@
--- COUNT() = Conta ocorrências.
-
--- MAX() = Retorna o maior valor de uma coluna.
-
--- MIN() = Retorna o menor valor de uma coluna.
-
--- AVG() = Retorna a média dos valores de uma coluna.
-
--- SUM() = Soma todos os valores de uma coluna.
