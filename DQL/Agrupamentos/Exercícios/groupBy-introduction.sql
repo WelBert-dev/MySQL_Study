00054edf81a5ac801bc32c788c2e3705ed39414d
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-09 01:24:48 -0300

    Update groupBy-introduction.sql

--- DQL/Agrupamentos/Exercícios/groupBy-introduction.sql
+++ DQL/Agrupamentos/Exercícios/groupBy-introduction.sql
@@ -1,25 +1,30 @@
--- Conceituando:
-
-
 CREATE TABLE vendas (
   id INT NOT NULL AUTO_INCREMENT,
-  nome_vendedor VARCHAR(100),
-  quantidade INT,
+  nome_vdedor VARCHAR(100),
+  qntd INT,
   produto VARCHAR(30),
   cidade VARCHAR(30),
   
   PRIMARY KEY (id)
 );
 
-INSERT INTO vendas (nome_vendedor, quantidade, produto, cidade) VALUES (10, 'Jorge', 1400, 'Mouse', 'São Paulo');
-INSERT INTO vendas (nome_vendedor, quantidade, produto, cidade) VALUES (12, 'Tatiana', 1220, 'Teclado', 'São Paulo');
-INSERT INTO vendas (nome_vendedor, quantidade, produto, cidade) VALUES (14, 'Ana', 1700, 'Teclado', 'Rio de Janeiro');
-INSERT INTO vendas (nome_vendedor, quantidade, produto, cidade) VALUES (15, 'Rita', 2120, 'Webcan', 'Recife');
-INSERT INTO vendas (nome_vendedor, quantidade, produto, cidade) VALUES (18, 'Marcos', 980, 'Mouse', 'São Paulo');
-INSERT INTO vendas (nome_vendedor, quantidade, produto, cidade) VALUES (19, 'Carla', 1120, 'Webcan', 'Recife');
-INSERT INTO vendas (nome_vendedor, quantidade, produto, cidade) VALUES (22, 'Roberto', 3145, 'Mouse', 'São Paulo');
+INSERT INTO vendas (id, nome_vdedor, qntd, produto, cidade) VALUES (10, 'Jorge', 1400, 'Mouse', 'São Paulo');
+INSERT INTO vendas (id, nome_vendedor, qntd, produto, cidade) VALUES (12, 'Tatiana', 1220, 'Teclado', 'São Paulo');
+INSERT INTO vendas (id, nome_vendedor, qntd, produto, cidade) VALUES (14, 'Ana', 1700, 'Teclado', 'Rio de Janeiro');
+INSERT INTO vendas (id, nome_vendedor, qntd, produto, cidade) VALUES (15, 'Rita', 2120, 'Webcan', 'Recife');
+INSERT INTO vendas (id, nome_vendedor, qntd, produto, cidade) VALUES (18, 'Marcos', 980, 'Mouse', 'São Paulo');
+INSERT INTO vendas (id, nome_vendedor, qntd, produto, cidade) VALUES (19, 'Carla', 1120, 'Webcan', 'Recife');
+INSERT INTO vendas (id, nome_vendedor, qntd, produto, cidade) VALUES (22, 'Roberto', 3145, 'Mouse', 'São Paulo');
 
-SELECT * 
-FROM vendas
-WHERE produto = 'Mouse';
+-- [vendas]:
+-- | id | nome_vdedor| qntd | produto | cidade         |
+-- | 10 | Jorge      | 1400 | Mouse   | São Paulo      |
+-- | 12 | Tatiana    | 1220 | Teclado | São Paulo      |
+-- | 14 | Ana        | 1700 | Teclado | Rio de Janeiro |
+-- | 15 | Rita       | 2120 | Webcan  | Recife         |
+-- | 18 | Marcos     | 980  | Mouse   | São Paulo      | 
+-- | 19 | Carla      | 1120 | Webcan  | Recife         |
+-- | 22 | Roberto    | 3145 | Mouse   | São Paulo      |
 
+-- GROUP BY: 
+   --
