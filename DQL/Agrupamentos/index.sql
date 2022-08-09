f5a631f766642d3f0c87533d7adc01461ab2a601
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-09 01:28:23 -0300

    Update index.sql

--- DQL/Agrupamentos/index.sql
+++ DQL/Agrupamentos/index.sql
@@ -1,4 +1,18 @@
 -- Conceituando: O agrupamento é bastante utilizado para agrupar valores, e também é muito utilizado em conjunto com funções de agregação!
 -- Para entender melhor iremos criar uma tabela vendas:
 
+-- Tabelas:
+-- [vendas]:
+-- | id | nome_vdedor| qntd | produto | cidade         |
+-- | 10 | Jorge      | 1400 | Mouse   | São Paulo      |
+-- | 12 | Tatiana    | 1220 | Teclado | São Paulo      |
+-- | 14 | Ana        | 1700 | Teclado | Rio de Janeiro |
+-- | 15 | Rita       | 2120 | Webcan  | Recife         |
+-- | 18 | Marcos     | 980  | Mouse   | São Paulo      | 
+-- | 19 | Carla      | 1120 | Webcan  | Recife         |
+-- | 22 | Roberto    | 3145 | Mouse   | São Paulo      |
+
+-- GROUP BY:
+
+
 
