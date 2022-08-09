3e933858da7e6c38c4a010fada3ec5211ed49549
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-09 01:27:10 -0300

    Update index.sql

--- DQL/Agrupamentos/index.sql
+++ DQL/Agrupamentos/index.sql
@@ -1,1 +1,4 @@
--- Conceituando: 
+-- Conceituando: O agrupamento é bastante utilizado para agrupar valores, e também é muito utilizado em conjunto com funções de agregação!
+-- Para entender melhor iremos criar uma tabela vendas:
+
+
