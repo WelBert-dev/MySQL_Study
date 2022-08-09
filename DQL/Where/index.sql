b62f862f602196cf158998221fd15c4a6ae92b62
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-09 19:18:37 -0300

    introduction and create WHERE folder

---
+++ DQL/Where/index.sql
@@ -0,0 +1,12 @@
+-- Conceituando: 
+-- Fonte: https://www.devmedia.com.br/clausula-where-e-seus-operadores-no-sql/17291
+
+-- Operadores utilizados em conjunto com a cláusula WHERE:
+  
+-- LIKE(%_)
+-- NOT LIKE(%_)
+-- IN (Lista_Valores)
+-- NOT IN
+-- OR
+-- BEETWEEN intervalo AND intervalo
+-- NOT BEETWEEN
