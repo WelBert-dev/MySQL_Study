c1463cf49834ff7a6617748505ac9130a791d301
welbert.dev <83488863+WelBert-dev@users.noreply.github.com>
2022-08-09 02:19:13 -0300

    Update index.sql

--- DQL/Agrupamentos/index.sql
+++ DQL/Agrupamentos/index.sql
@@ -35,13 +35,48 @@ GROUP BY colunas;
    -- | 19 | Carla      | 1120   | Webcan  | Recife         |
  ---->| 22 | Roberto    | [ 3145 | Mouse ] | São Paulo      |<----
    -- Saída: 
+      -- | Total_Mouses | 
+      -- | 5525         |
 
 
 -- Exemplo 2 (GROUP BY com SUM): Consulta totalizando as vendas de produtos POR cidade;
-   SELECT cidade, SUM(qntd) as Total_vendas
+   SELECT cidade, SUM(qntd) as Total_Vendas
    FROM vendas
    GROUP BY cidade;
-   -- [1a Etapa()]:
+   -- [1a Etapa(GROUP BY cidade) quebra e organiza por cidade (grupos de cidade)]:
+   -- [vendas]:
+   -- | id | nome_vdedor| qntd | produto | [ cidade     ] |
+   -- | 10 | Jorge      | 1400 | Mouse   | São Paulo      |
+   -- | 12 | Tatiana    | 1220 | Teclado | São Paulo      |
+   -- | 18 | Marcos     | 980  | Mouse   | São Paulo      |
+   -- | 22 | Roberto    | 3145 | Mouse   | São Paulo      |
+
+   -- | 15 | Rita       | 2120 | Webcan  | Recife         | 
+   -- | 19 | Carla      | 1120 | Webcan  | Recife         |
+
+   -- | 14 | Ana        | 1700 | Teclado | Rio de Janeiro |
+
+   -- [2a Etapa(SUM(qntd) as Total_Vendas) Aplica para cada grupo a soma das colunas]:
+   -- [vendas]:
+   -- | id | nome_vdedor| [ qntd ] | produto | cidade         |
+   -- | 10 | Jorge      | 1400     | Mouse   | São Paulo      |
+   -- | 12 | Tatiana    | 1220     | Teclado | São Paulo      |
+   -- | 18 | Marcos     | 980      | Mouse   | São Paulo      |
+   -- | 22 | Roberto    | 3145     | Mouse   | São Paulo      |
+ ----------------------> [ 6745 ] <-------------------------------
+        
+   -- | 15 | Rita       | 2120     | Webcan  | Recife         | 
+   -- | 19 | Carla      | 1120     | Webcan  | Recife         |
+ ----------------------> [ 3240 ] <-------------------------------
+
+   -- | 14 | Ana        | 1700     | Teclado | Rio de Janeiro |
+ ----------------------> [ 1700 ] <-------------------------------
+
+   -- Saída (Organiza em ASC):
+      -- | Cidade         | Total_vendas |
+      -- | Recife         | 3240         |
+      -- | Rio de Janeiro | 1700         |
+      -- | São Paulo      | 6745         |
 
 
 
